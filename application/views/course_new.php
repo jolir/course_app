bdf8144996e1f80e6b3458d91eda6a021d619c4b
jolir <oliverjohnpr2011@yahoo.com>
2013-07-25 08:00:53 -0700

    cleaned up some codes. still doesnt have edit feature

--- application/views/course_new.php
+++ application/views/course_new.php
@@ -6,6 +6,7 @@
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
+
 			$('#new_course').submit(function(){
 				var form = $(this);
 				$.post(form.attr('action'), form.serialize(), function(data){
@@ -16,20 +17,7 @@
 				return false;
 			});
 
-			// $('.edit').click(function(){
-			// 	var edit = $(this);
-			// 	var description_text = edit.parent().siblings().text();
-			// 	var description_location = edit.parent().siblings();
-			// 	var title_text = edit.parent().parent().parent().siblings().children().text();
-			// 	var title_location = edit.parent().parent().parent().siblings().children();
-
-			// 	description_location.html($('<textarea />',{'value' : description_text}).val(description_text));
-			// 	title_location.html($('<input />', {'value' : title_text}).val(title_text));
-			// 	edit.text("Save");					
-
-			// });
-
-			$('#accordion2').on("click", ".delete_course button", function(event){
+			$('#accordion').on("click", ".delete_course button", function(event){
 				var form = $(this).parent();
 				$.post(form.attr('action'), form.serialize(), function(data){
 					console.log(data);
@@ -55,33 +43,32 @@
 	</div>
 	<div style="clear: both;"></div>
 
-	<div class="accordion span8" id="accordion2">
-<? 		foreach($courses as $course) 
-		{ ?>
-			<div class="accordion-group">
-				<div class="accordion-heading" id="course_group<?= $course['id']; ?>">
-					<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#course<?= $course['id']; ?>"><?= $course['title']; ?></a>
-
-				</div>
-				<div id="course<?= $course['id']; ?>" class="accordion-body collapse">
-					<div class="accordion-inner">
-						<p><?= $course['description']; ?></p>
-						<form action="courses/delete_course" method="post" class="delete_course pull-right">
-							<input type="hidden" name="form_action" value="delete_course">
-							<input type="hidden" name="course_id" value="<?= $course['id']; ?>">
-							<button class="delete">delete</button> 
-						</form>
-						<form action="courses/delete_course" method="post" class="pull-right">
-							<input type="hidden" name="form_action" value="delete_course">
-							<input type="hidden" name="course_id" value="<?= $course['id']; ?>">
-							<input type="hidden" name="course_title" value="<?= $course['title']; ?>">
-							<input type="hidden" name="course_description" value="<?= $course['description']; ?>">
-							<button class=" edit" style="margin-right: 5px;">edit</button>
-						</form>
-					</div>
+	<div class="accordion span8" id="accordion">
+<? 	foreach($courses as $course) 
+	{ ?>
+		<div class="accordion-group">
+			<div class="accordion-heading" id="course_group<?= $course['id']; ?>">
+				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#course<?= $course['id']; ?>"><?= $course['title']; ?></a>
+			</div>
+			<div id="course<?= $course['id']; ?>" class="accordion-body collapse">
+				<div class="accordion-inner">
+					<p><?= $course['description']; ?></p>
+					<form action="courses/delete_course" method="post" class="delete_course pull-right">
+						<input type="hidden" name="form_action" value="delete_course">
+						<input type="hidden" name="course_id" value="<?= $course['id']; ?>">
+						<button class="delete">delete</button> 
+					</form>
+					<form action="courses/delete_course" method="post" class="pull-right">
+						<input type="hidden" name="form_action" value="delete_course">
+						<input type="hidden" name="course_id" value="<?= $course['id']; ?>">
+						<input type="hidden" name="course_title" value="<?= $course['title']; ?>">
+						<input type="hidden" name="course_description" value="<?= $course['description']; ?>">
+						<button class=" edit" style="margin-right: 5px;">edit</button>
+					</form>
 				</div>
 			</div>
-<? 		} ?>
+		</div>
+<? 	} ?>
 	</div>
 </body>
 </html>
--- application/views/home_page.php
+++ application/views/home_page.php
@@ -4,14 +4,10 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 </head>
 <body>
-<div class="hero-unit">
-  <h1>Simple Coures Outline Application</h1>
-  <p>Simple course outline application</p>
-  <p>
-    <a class="btn btn-primary btn-large" href="courses">
-      Try it out!
-    </a>
-  </p>
-</div>
+	<div class="hero-unit">
+		<h1>Simple Coures Outline Application</h1>
+		<p>Simple course outline application</p>
+		<p><a class="btn btn-primary btn-large" href="courses">Try it out!</a></p>
+	</div>
 </body>
 </html>
